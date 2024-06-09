73932e8e8b595ae8ddcf3640445278c8fcde61b3
RobertineIFC <76454392+RobertineIFC@users.noreply.github.com>
2024-06-09 14:36:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const fleetData = [
         registration: 'N638OD',
         type: 'P-38',
         color: 'Purple',
-        airports: ['KVCV', 'KCOS', 'KOGA', 'KFSD', 'KMWM', 'KOSH', 'CNK4', 'CYYB']
+        airports: ['KVCV', 'KTEX', 'KCOS', 'KOGA', 'KFSD', 'KMWM', 'KOSH', 'CNK4', 'CYYB']
     },
     {
         registration: 'N639TF',
@@ -79,6 +79,7 @@ const airportCoords = {
     'U85': { coords: [43.4919, -111.9042], name: 'Rexburg-Madison County Airport' },
     'KYKM': { coords: [46.5682, -120.543], name: 'Yakima Air Terminal' },
     'CYYB': { coords: [46.362976, -79.424633], name: 'Jack Garland Airport' },
+    'KTEX': { coords: [37.953894, -107.901981], name: 'Telluride Regional Airport' },
 };
 
 var map = L.map('map').setView([39.8283, -98.5795], 4);
@@ -94,31 +95,24 @@ var polylines = [];
 var markers = [];
 var airplaneIcons = [];
 
-
 function resetMapView() {
     polylines.forEach(line => map.addLayer(line));
     markers.forEach(marker => map.addLayer(marker));
     airplaneIcons.forEach(icon => map.addLayer(icon));
 }
 
-
 fleetData.forEach(plane => {
     const coordinates = plane.airports.map(airport => airportCoords[airport].coords);
 
     const polyline = L.polyline(coordinates, { color: colorMap[plane.color], weight: 5 })
         .on('click', function (e) {
-
             L.DomEvent.stopPropagation(e);
 
-
             polylines.forEach(line => map.removeLayer(line));
-
             map.addLayer(polyline);
-
 
             markers.forEach(marker => map.removeLayer(marker));
             airplaneIcons.forEach(icon => map.removeLayer(icon));
-
 
             plane.airports.forEach(airport => {
                 const marker = markers.find(m => m.getLatLng().equals(airportCoords[airport].coords));
@@ -138,14 +132,16 @@ fleetData.forEach(plane => {
 
     plane.airports.forEach(airport => {
         const { coords, name } = airportCoords[airport];
+        const isPlaneHere = plane.airports[plane.airports.length - 1] === airport;
+        const planeText = isPlaneHere ? plane.registration : 'No aircraft';
         const marker = L.marker(coords)
-            .bindPopup(`<b>${name}</b><br>ICAO: ${airport}`)
+            .bindPopup(`<b>${name}</b><br>ICAO: ${airport}<br>Planes: ${planeText}`)
             .addTo(map);
         markers.push(marker);
     });
 
-
-    const currentLocation = airportCoords[plane.airports[plane.airports.length - 1]];
+    const currentAirport = plane.airports[plane.airports.length - 1];
+    const { coords } = airportCoords[currentAirport];
     const airplaneIcon = L.icon({
         iconUrl: 'airplane.png',
         iconSize: [32, 32],
@@ -153,8 +149,8 @@ fleetData.forEach(plane => {
         popupAnchor: [0, -16]
     });
 
-    const airplaneMarker = L.marker(currentLocation.coords, { icon: airplaneIcon })
-        .bindPopup(`<b>Current Location</b><br>${currentLocation.name}<br>ICAO: ${plane.airports[plane.airports.length - 1]}`)
+    const airplaneMarker = L.marker(coords, { icon: airplaneIcon })
+        .bindPopup(`<b>Current Location</b><br>${airportCoords[currentAirport].name}<br>ICAO: ${currentAirport}`)
         .addTo(map);
 
     airplaneIcons.push(airplaneMarker);
@@ -163,3 +159,62 @@ fleetData.forEach(plane => {
 map.on('click', function () {
     resetMapView();
 });
+
+
+function toggleMapLayer() {
+    if (map.hasLayer(satelliteLayer)) {
+        map.removeLayer(satelliteLayer);
+        map.addLayer(normalLayer);
+    } else {
+        map.removeLayer(normalLayer);
+        map.addLayer(satelliteLayer);
+    }
+}
+
+
+var MapLayerControl = L.Control.extend({
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+
+        this._toggleButton = this._createButton('Toggle Map', 'Toggle between satellite and normal map', 'leaflet-control-toggle', container, toggleMapLayer);
+
+        return container;
+    },
+
+    _createButton: function (text, title, className, container, callback) {
+        var button = L.DomUtil.create('a', className, container);
+        button.innerHTML = text;
+        button.href = '#';
+        button.title = title;
+
+        button.style.width = 'auto'; 
+        button.style.height = '30px'; 
+        button.style.lineHeight = '30px';
+        button.style.padding = '0 15px'; 
+
+        L.DomEvent.on(button, 'click', L.DomEvent.stop)
+            .on(button, 'click', callback, this);
+
+        return button;
+    }
+});
+
+
+var mapLayerControl = new MapLayerControl();
+
+
+mapLayerControl.addTo(map);
+
+
+var satelliteLayer = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3'],
+    attribution: '&copy; <a href="https://www.google.com/maps">Google Maps</a>'
+});
+
+var normalLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+
+satelliteLayer.addTo(map);
